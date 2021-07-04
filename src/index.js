1275fe05f95d8fb46a37cca26142f7ac6f6f3d1f
Andrey Serikov <as.korag@protonmail.com>
2021-07-05 00:23:15 +0300

    add : findEmptyPlace function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku(board) {
+  const BOARD_SIZE = board.length
+  const SECTOR_SIZE = Math.sqrt(BOARD_SIZE)
+}
+
+function findEmptyPlace(board) {
+  board.forEach((row, rowIndex) => {
+    row.forEach((num, colIndex) => {
+      if (num === 0) {
+        return { row: rowIndex, col: colIndex }
+      }
+    })
+  })
+
+  return null
 }
