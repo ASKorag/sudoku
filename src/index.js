1ffabd1809f7de8b446cab49cacc7195dfe4d196
Andrey Serikov <AS.Korag@protonmail.com>
2021-07-05 11:30:49 +0300

    add: checkNum function

--- src/index.js
+++ src/index.js
@@ -7,10 +7,47 @@ function findEmptyPlace(board) {
   board.forEach((row, rowIndex) => {
     row.forEach((num, colIndex) => {
       if (num === 0) {
-        return { row: rowIndex, col: colIndex }
+        return { rowIndex, colIndex }
       }
     })
   })
 
   return null
+}
+
+function checkNum(num, pos, board) {
+  const { rowIndex, colIndex } = pos
+
+  //-----------Check Row------------
+
+  board[rowIndex].forEach((value, index) => {
+    if (value === num && index !== colIndex) {
+      return false
+    }
+  })
+
+  //-----------Check Column------------
+
+  board.forEach((row, index) => {
+    const value = row[colIndex]
+
+    if (value === num && index !== rowIndex) {
+      return false
+    }
+  })
+
+  //-----------Check Sector------------
+
+  const sectorBeginRow = SECTOR_SIZE * Math.floor(rowIndex / SECTOR_SIZE)
+  const sectorBeginCol = SECTOR_SIZE * Math.floor(colIndex / SECTOR_SIZE)
+
+  for (let r = sectorBeginRow; r < sectorBeginRow + SECTOR_SIZE; r++) {
+    for (let c = sectorBeginCol; c < sectorBeginCol + SECTOR_SIZE; c++) {
+      if (board[r][c] === num && r !== rowIndex && c !== colIndex) {
+        return false
+      }
+    }
+  }
+
+  return true
 }
